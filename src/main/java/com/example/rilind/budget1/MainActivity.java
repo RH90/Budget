dbdf0d8f0d1443532eed0d5a2c71e281fa3d73ce
Rilind Hasanaj <Rilind Hasanaj>
2017-04-08 00:39:38 +0200

    v0

---
+++ src/main/java/com/example/rilind/budget1/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.rilind.budget1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MainActivity extends AppCompatActivity {
+    static String check = "";
+    EditText editText;
+    static TextView textView;
+    static String message = "";
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        textView = (TextView) findViewById(R.id.textView);
+        //textView.setMovementMethod(new ScrollingMovementMethod());
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("event"));
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final SQL fc = new SQL();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    fc.start(getApplicationContext());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            textView = (TextView) findViewById(R.id.textView);
+            message = intent.getStringExtra("message");
+            textView.append(message);
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+  //  public void button(View view) throws IOException {
+  //      editText = (EditText) findViewById(R.id.ediText);
+  //      check = String.valueOf(editText.getText());
+  //  }
+
+    public static void edit(String s) {
+        textView.append(s);
+    }
+}
---
+++ src/main/java/com/example/rilind/budget1/SQL.java
@@ -0,0 +1,117 @@
+package com.example.rilind.budget1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import junit.runner.Version;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Rilind on 2017-04-07.
+ */
+
+public class SQL {
+
+    Intent intent = new Intent("event");
+    Context context = null;
+
+    public  void start(Context context) throws SQLException, ClassNotFoundException, IOException {
+        String s ;
+        this.context=context;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            s = "wwwqq";
+            intent.putExtra("message", s);
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            while (!MainActivity.message.equals(s)) {
+
+            }
+
+        }
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection con = null;
+        try {
+
+            con = DriverManager.getConnection("jdbc:mysql://192.168.1.8:3306/world", "RH9011", "RH9011");
+            stmt = con.createStatement();
+            String query = "select * from city";
+            rs = stmt.executeQuery(query);
+            ResultSetMetaData columns = rs.getMetaData();
+
+            s = String.format("%4s | %-34s | %3s | %-10s\n", columns.getColumnName(1), columns.getColumnName(2), columns.getColumnName(3), columns.getColumnName(4));
+            intent.putExtra("message", s);
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            while (!MainActivity.message.equals(s)) {
+
+            }
+
+
+            //System.out.printf("%4s | %-34s | %3s | %-10s\n", columns.getColumnName(1), columns.getColumnName(2), columns.getColumnName(3), columns.getColumnName(4));
+            //System.out.println("------------------------------------------------------------------");
+            int a = 0, b = 0, c = 0, d = 0;
+
+            while (rs.next()) {
+                if (rs.getString(1).length() > a) {
+                    a = rs.getString(1).length();
+                }
+                if (rs.getString(2).length() > b) {
+                    b = rs.getString(2).length();
+                }
+                if (rs.getString(3).length() > c) {
+                    c = rs.getString(3).length();
+                }
+                if (rs.getString(4).length() > d) {
+                    d = rs.getString(4).length();
+                }
+            }
+
+            rs.beforeFirst();
+            while (rs.next()) {//get first result
+                s = String.format("%1$-" + a + "s | %2$-" + b + "s | %3$-" + 11 + "s | %4$-" + d + "s\n", rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
+                intent.putExtra("message", s);
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+                while (!MainActivity.message.equals(s)) {
+
+                }
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Login fail");
+        } finally {
+            s = String.format("Hej");
+            intent.putExtra("message", s);
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            while (!MainActivity.message.equals(s)) {
+
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+
+}
